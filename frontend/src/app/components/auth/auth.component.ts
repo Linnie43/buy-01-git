dee1447eda93c4e63e9c0b1e640fd28d412882c6
Linnea Gabrielsson <annalinneagabrielsson@gmail.com>
2025-10-28 15:54:04 +0200

    added password validation field

--- frontend/src/app/components/auth/auth.component.ts
+++ frontend/src/app/components/auth/auth.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+
+export function passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    return { passwordsMismatch: true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-auth',
@@ -20,12 +30,13 @@ export class AuthComponent {
   });
 
   signupForm = this.fb.group({
-    firstname: ['', [Validators.required, Validators.minLength(2)]],
-    lastname: ['', [Validators.required, Validators.minLength(2)]],
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(3)]],
-    role: ['CLIENT', Validators.required]
-  });
+      firstname: ['', [Validators.required, Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]],
+      confirmPassword: ['', Validators.required],
+      role: ['CLIENT', Validators.required]
+    }, { validators: passwordsMatchValidator });
 
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
 

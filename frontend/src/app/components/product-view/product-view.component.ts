78795cd3b0593640de0e22558789c94b34267a88
Maris Karu <maris.karu@gritlab.ax>
2026-01-05 14:12:16 +0200

    added the quantity selector functionality

--- frontend/src/app/components/product-view/product-view.component.ts
+++ frontend/src/app/components/product-view/product-view.component.ts
@@ -7,21 +7,27 @@ import { AuthService } from '../../services/auth.service';
 import { ImageUrlPipe } from '../../pipes/image-url.pipe';
 import { ImageCarouselComponent } from '../shared/image-carousel/image-carousel.component';
 import { HttpErrorResponse } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { CartService } from '../../services/cart.service';
+import { FormsModule } from '@angular/forms';
+import { CartResponseDTO } from '../../models/cart.model';
 
 @Component({
   selector: 'app-product-view',
   standalone: true,
-  imports: [CommonModule, ImageUrlPipe, ImageCarouselComponent],
+  imports: [CommonModule, FormsModule, ImageUrlPipe, ImageCarouselComponent],
   templateUrl: './product-view.component.html',
   styleUrl: './product-view.component.css'
 })
 export class ProductViewComponent implements OnInit {
   product: Product | null = null;
   isLoggedIn = false;
+  selectedQuantity = 1;
 
   constructor(
     private route: ActivatedRoute,
     private productService: ProductService,
+    private cartService: CartService,
     private authService: AuthService,
     private router: Router
   ) {}
@@ -50,4 +56,22 @@ export class ProductViewComponent implements OnInit {
   goToUpdateProduct(productId: string) {
     this.router.navigate(['/products/update', productId]);
   }
+
+  addToCart() {
+      if (!this.product) return;
+
+      this.cartService.addToCart({
+        productId: this.product.productId,
+        quantity: this.selectedQuantity
+      }).subscribe({
+        next: (res: CartResponseDTO) => {
+          console.log('Added to cart', res);
+          alert('Product added to cart!');
+        },
+        error: (err) => {
+          console.error('Cannot add to cart', err);
+          alert('Cannot add more than available quantity!');
+        }
+      });
+    }
 }

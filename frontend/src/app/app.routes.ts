a2efde58856c2f1e78a7ad3f54906c27fecbaf24
fvesanen <fanni.vesanen@gmail.com>
2026-01-09 12:25:59 +0200

    added baseURLs to constants

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
 import { ProductViewComponent } from './components/product-view/product-view.component';
 import { ManageProductsComponent } from './components/manage-products/manage-products.component';
+import { OrderViewComponent } from './components/order-view/order-view.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { productOwnerGuard } from './guards/product-owner.guard';
 import { SessionGuard } from './guards/session.guard';
@@ -19,6 +20,7 @@ export const routes: Routes = [
   { path: 'products/:id', component: ProductViewComponent },
   { path: 'seller-profile', component: SellerProfileComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'SELLER' }  },
   { path: 'client-profile', component: ClientProfileComponent, canActivate: [AuthGuard, RoleGuard], data: { role: 'CLIENT' }  },
+  { path: 'order/:id', component: OrderViewComponent, canActivate: [AuthGuard]},
   { path: '404', component: NotFoundComponent },
   { path: '**', redirectTo: '/404' }
 ];
